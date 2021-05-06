7808d09efe3228f942ce15fe6740626c67f2718f
Andrew Masih <armasih@outlook.com>
2021-05-06 22:08:03 +0000

    added comments

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ let country = {
 };
 
 /* we can create objects by simply using a variable name of the object and assigning brackets to the varible*/
+
+/* pbjects are like a dictionary of keys and values where the keys are the words and the values are the definintion */
+
+/* in the dictionary you can look up a word and find its defination, similary, with an object you can use its keys to look up values, which can be data like numbers, strings, varibles, function and objects */
