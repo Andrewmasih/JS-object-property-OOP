86dc3f11a30121a5e958e1d857865d5a0bd877f6
Andrew Masih <armasih@outlook.com>
2021-05-06 11:44:37 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/* a collection of related data, it holds data, methods, properties, functions, and even other data  */
+
+let country = {
+	name: "Croatia",
+	population: 4.224,
+	sea: true,
+	islands: 1000,
+	language: "croatian",
+};
+
+/* we can create objects by simply using a variable name of the object and assigning brackets to the varible*/
